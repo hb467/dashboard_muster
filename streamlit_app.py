6267b92720a2a0622e3c6476237b4f45358b583a
hb467 <baykarahuseyin@yahoo.com>
2024-12-02 16:23:31 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,45 +2,48 @@ import streamlit as st
 import pandas as pd
 from datetime import datetime, time
 
-# Initialdaten (leere Tabelle)
-if "entries" not in st.session_state:
-    st.session_state.entries = pd.DataFrame(
-        columns=["FIN", "Produktvariante", "Im Takt?", "Fehlercode", "Bemerkung", "Qualität", "Meldezeit", "Taktzeit"]
-    )
+# Initialisierung des Session States für die Tabelle
+if "data" not in st.session_state:
+    st.session_state.data = pd.DataFrame(columns=[
+        "FIN", "Produktvariante", "Im Takt?", "Fehlercode", "Bemerkung", "Qualität", "Meldezeit", "Taktzeit"
+    ])
 
-# Layout und Titel
+# Header mit Logo und Titel
 st.image("https://www.brueggen.com/fileadmin/_processed_/6/1/csm_logo_c6de901564.png", width=300)
 st.title("Produktionsdokumentation")
 
-# Spalten für Eingaben
-col1, col2, col3, col4 = st.columns(4)
+# Eingabeformulare für allgemeine Informationen
+with st.form("general_form"):
+    col1, col2, col3, col4 = st.columns(4)
 
-with col1:
-    datum = st.date_input("Datum", value=datetime.now())
-with col2:
-    start = st.time_input("Start", value=time(14, 0))
-with col3:
-    schicht = st.selectbox("Schicht", ["Früh", "Spät", "Nacht"])
-with col4:
-    ende = st.time_input("Ende", value=time(22, 0))
+    with col1:
+        datum = st.date_input("Datum", value=datetime.now())
+    with col2:
+        start = st.time_input("Start", value=time(14, 0))
+    with col3:
+        schicht = st.selectbox("Schicht", ["Früh", "Spät", "Nacht"])
+    with col4:
+        ende = st.time_input("Ende", value=time(22, 0))
 
-# Weitere Eingaben
-col5, col6, col7, col8 = st.columns(4)
+    # Weitere Produktionsdaten
+    col5, col6, col7, col8 = st.columns(4)
 
-with col5:
-    ausschuss = st.text_input("Ausschuss", value="Ausschuss")
-with col6:
-    schicht_soll = st.text_input("Schicht-SOLL", value="Schicht-SOLL")
-    soll_aktuell = st.text_input("Soll Aktuell", value="Soll Aktuell")
-with col7:
-    ist_aktuell = st.text_input("Ist Aktuell", value="Ist Aktuell")
-    rückstand = st.text_input("Rückstand", value="Rückstand")
-with col8:
-    direktläufer = st.text_input("Direktläufer", value="Direktläufer")
+    with col5:
+        ausschuss = st.text_input("Ausschuss", value="0")
+    with col6:
+        schicht_soll = st.text_input("Schicht-SOLL", value="0")
+    with col7:
+        soll_aktuell = st.text_input("Soll Aktuell", value="0")
+        ist_aktuell = st.text_input("Ist Aktuell", value="0")
+    with col8:
+        rückstand = st.text_input("Rückstand", value="0")
+        direktläufer = st.text_input("Direktläufer", value="0")
 
-# Eingabebereich für neue Daten
-st.header("Eingabe Sattelhals")
+    submitted_general = st.form_submit_button("Schichtdaten speichern")
+
+# Eingabeformular für spezifische Daten
 with st.form("entry_form"):
+    st.subheader("Eingabe Sattelhals")
     fin = st.text_input("FIN")
     produktvariante = st.selectbox("Produktvariante", ["Standard", "RoRo", "Co2"])
     im_takt = st.radio("Im Takt gefertigt?", ["Ja", "Nein"], horizontal=True)
@@ -49,9 +52,10 @@ with st.form("entry_form"):
     qualität = st.radio("Qualität", ["i.O.", "e.i.O.", "n.i.O."], horizontal=True)
     meldezeit = st.time_input("Meldezeit", value=datetime.now().time())
     taktzeit = st.text_input("Taktzeit", value="00:25:30")
-    submitted = st.form_submit_button("Eintrag hinzufügen")
 
-    if submitted:
+    submitted_entry = st.form_submit_button("Eintrag hinzufügen")
+
+    if submitted_entry:
         new_entry = {
             "FIN": fin,
             "Produktvariante": produktvariante,
@@ -62,18 +66,17 @@ with st.form("entry_form"):
             "Meldezeit": str(meldezeit),
             "Taktzeit": taktzeit,
         }
-        st.session_state.entries = pd.concat([st.session_state.entries, pd.DataFrame([new_entry])], ignore_index=True)
+        st.session_state.data = pd.concat([st.session_state.data, pd.DataFrame([new_entry])], ignore_index=True)
         st.success("Eintrag hinzugefügt!")
 
-# Tabelle anzeigen
-st.header("Schichtübersicht")
-st.table(st.session_state.entries)
+# Tabelle mit den Daten
+st.subheader("Schichtübersicht")
+st.table(st.session_state.data)
 
 # Letzten Eintrag löschen
 if st.button("Letzten Eintrag löschen"):
-    if not st.session_state.entries.empty:
-        st.session_state.entries = st.session_state.entries.iloc[:-1]
+    if not st.session_state.data.empty:
+        st.session_state.data = st.session_state.data.iloc[:-1]
         st.success("Letzter Eintrag gelöscht!")
     else:
         st.warning("Keine Einträge vorhanden!")
-
