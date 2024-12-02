b0e873e9b12fcf860881ffd86ddb148199fafb2d
hb467 <baykarahuseyin@yahoo.com>
2024-12-02 16:08:15 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # HTML-Datei laden
-html_file = "index(1).html"  # Pfad zur Datei
+html_file = "data/index.html"  # Pfad zur Datei
 
 # HTML-Inhalt lesen
 with open(html_file, "r", encoding="utf-8") as file:
